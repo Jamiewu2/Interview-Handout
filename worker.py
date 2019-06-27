4fcf9c2ede487bbfe64cece48d5d9c4bdd3635cc
Ryan Archer <rlarcher@alumni.cmu.edu>
2019-06-27 14:47:22 -0400

    Update worker.py
    yayay 👍

--- worker.py
+++ worker.py
@@ -2,23 +2,23 @@ from time import sleep
 import random
 import mock_db
 
-text = 'Maestro is the best......'
+text = 'Maestro is the best......\n\n'
 
 def write_line(file_name, line):
-    with open(file_name, 'a') as file:
-        file.write(line)
+    with open(file_name, 'a') as f:
+        f.write(line)
 
 def worker_main(worker_hash, db, cb):
     print("In worker main")
     should_crash = random.random()
-    if should_crash < 0.0:
+    if should_crash < 0.5:
         raise Exception("Crash")
-    cursor = 0
-    text_to_write = f'{text}\n\n'
-    while cursor < len(text_to_write):
+        
+    cursor = 0   
+    while cursor < len(text):
         start = cursor
-        end = min(cursor + 5, len(text_to_write))
-        write_line('./output.txt', text_to_write[start: end])
+        end = min(cursor + 5, len(text))
+        write_line('output.txt', text[start: end])
         sleep(2)
         cursor += 5
     cb()
