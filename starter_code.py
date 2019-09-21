e579330d36acc36248685c7516d390cbe175517b
Jamie Wu <jamiewu2@illinois.edu>
2019-09-21 13:43:37 -0400

    some refactoring and bugfix

--- starter_code.py
+++ starter_code.py
@@ -80,7 +80,7 @@ def single_threaded(func, lock_key):
                 time.sleep(0.1)
 
         try:
-            func(*args, **kwargs)
+            return func(*args, **kwargs)
         finally:
             db.delete_one({'_id': lock_key})
 
@@ -91,6 +91,11 @@ DATABASE_LOCK_KEY = "database_lock_key"
 
 
 def _get_job_from_db(db, worker_hash) -> Job:
+    """
+    :param db:
+    :param worker_hash:
+    :return: get the job object from db, or raise exception if 'worker_hash' not found
+    """
     job_dict = db.find_one({'_id': worker_hash})
     if not job_dict:
         raise Exception(f"job: {worker_hash} not found")
@@ -100,12 +105,25 @@ def _get_job_from_db(db, worker_hash) -> Job:
 @single_threaded(DATABASE_LOCK_KEY)
 def add_to_queue(db, worker_hash):
     timestamp = datetime.now().timestamp()
-
     job = Job(worker_hash, timestamp)
+
+    # safety check
+    first_job = _get_first_job_in_queue(db)
+    if first_job and timestamp < first_job.job_submit_time:
+        raise Exception("Attempting to add a job into the front of the queue")
+
     db.insert_one(job.to_dict())
 
 
 def update_job(db, worker_hash, job_status, error_message=None):
+    """
+    Update the job in the db with a job status, and optionally an error message
+    :param db:
+    :param worker_hash:
+    :param job_status:
+    :param error_message:
+    :return:
+    """
     job = _get_job_from_db(db, worker_hash)
     job.status = job_status
     job.job_run_time = datetime.now().timestamp() - job.job_submit_time
@@ -115,19 +133,30 @@ def update_job(db, worker_hash, job_status, error_message=None):
     db.update_one({'_id': job._id}, job.to_dict())
 
 
+def _get_first_job_in_queue(db):
+    """
+    :param db:
+    :return: Returns the first job in the queue, or None if the queue is empty
+    """
+    jobs = db.find_many({"status": JobStatus.PENDING.value})
+    if not jobs:
+        return None
+
+    jobs = map(Job.from_dict, jobs)
+
+    first_job = sorted(jobs, key=lambda job: job.job_submit_time)[0]
+    return first_job
+
+
 def lock_is_free(db, worker_hash):
     """
         CHANGE ME, POSSIBLY MY ARGS
 
         Return whether the lock is free
     """
-    jobs = db.find_many({"status": JobStatus.PENDING.value})
-    jobs = map(Job.from_dict, jobs)
-
-    if jobs:
-        first_job = sorted(jobs, key=lambda job: job.job_submit_time)[0]
-        if first_job._id == worker_hash:
-            return True
+    first_job = _get_first_job_in_queue(db)
+    if first_job and first_job._id == worker_hash:
+        return True
 
     return False
 
@@ -173,7 +202,6 @@ def attempt_run_worker(worker_hash, give_up_after, db, retry_interval):
                 worker_main(worker_hash, db)
                 update_job(db, worker_hash, JobStatus.SUCCESS)
                 write_line("output.txt", "")
-                print(db.store)
                 return
 
         except Exception as e:
